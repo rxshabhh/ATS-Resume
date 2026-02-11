d78fb4b0e0dc3cd373768eaad90a38e683990c23
allubhujia <veervikram451@gmail.com>
2026-02-11 13:37:30 +0000

    Testing analyze webpage checking if its working or not

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Upload from './pages/Upload';
 import LearnMore from './pages/LearnMore';
+import Analyze from './pages/Analyze';
+
 function App() {
   return (
     <Router>
