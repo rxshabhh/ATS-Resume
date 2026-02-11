de16a1db104b7895f7fa6ac8c13c0b197b0ac1c3
allubhujia <78474288+allubhujia@users.noreply.github.com>
2026-02-11 19:09:28 +0530

    Add Navbar component to Analyze page

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -4,9 +4,11 @@ import Background from '../components/Background'
 function Analyze(){
     return(
         <Background>
+            <Navbar />
 
         </Background>
     )
 }
 
 export default Analyze
+
