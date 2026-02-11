0fda96ec811b5373470031e42dea94ea9c78ccca
allubhujia <veervikram451@gmail.com>
2026-02-11 13:37:30 +0000

    Testing analyze webpage checking if its working or not

---
+++ src/pages/Analyze.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+import Background from '../components/Background'
+
+function Analyze(){
+    return(
+        <Background>
+
+        </Background>
+    )
+}
+
+export default Analyze
